31cda1465c87cd2d7cc58e1cac5cf4dcb71942b6
Amii Jiang <amii@192.168.0.107>
2023-04-06 07:40:09 +0800

    first commit

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,1 +1,20 @@
 # write your bubble sort algorithm below
+
+def bubble_sort(lst):
+    for i in range(len(lst)-1):
+        swapped = False
+        print(f"iteration ")
+        for j in range(len(lst)-1):
+            print(f"comparing {lst[j], lst[j+1]}")
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1] , lst[j]
+                swapped = True
+        if not swapped:
+            return
+    return lst
+
+sample_list = [1,5,2,6,7]
+print(f"Unsorted list: {sample_list}")
+bubble_sort(sample_list)
+
+print(f"Sorted list: {sample_list}")
